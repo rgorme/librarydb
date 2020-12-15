908e75674ec130803e704db29b5853a7ee7591e7
Rasmus Gorsmbøl Larsen <rasmus@gormsboel.dk>
2020-12-15 18:02:05 +0100

    xx

--- src/data/authors/index.js
+++ src/data/authors/index.js
@@ -12,33 +12,33 @@ const register = async ( { sql, getConnection } ) => {
         return request.query(sqlQueries.getAuthors);
     };
 
-   const addAuthor = async ( { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION } ) => {
-        const pool = await getConnection();
-        const request = await pool.request();
-        request.input( "FIRST_NAME", sql.VarChar(250), FIRST_NAME);
-        request.input( "LAST_NAME", sql.VarChar(250), LAST_NAME);
-        request.input( "NATIONALITY_SHORT", sql.VarChar(5), NATIONALITY_SHORT);
-        request.input( "DESCRIPTION", sql.VarChar, DESCRIPTION);
-        return request.query( sqlQueries.addAuthor );
-   };
+   async function addAuthor(FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION ) {
+          const pool = await getConnection();
+          const request = await pool.request();
+          request.input("FIRST_NAME", sql.VarChar(250), FIRST_NAME);
+          request.input("LAST_NAME", sql.VarChar(250), LAST_NAME);
+          request.input("NATIONALITY_SHORT", sql.VarChar(5), NATIONALITY_SHORT);
+          request.input("DESCRIPTION", sql.VarChar, DESCRIPTION);
+          return request.query(sqlQueries.addAuthor);
+     }
 
-   const updateAuthor = async ( { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION, AUTHOR_ID } ) => {
-        const pool = await getConnection();
-        const request = await pool.request();
-        request.input( "FIRST_NAME", sql.VarChar(250), FIRST_NAME);
-        request.input( "LAST_NAME", sql.VarChar(250), LAST_NAME);
-        request.input( "NATIONALITY_SHORT", sql.VarChar(5), NATIONALITY_SHORT);
-        request.input( "DESCRIPTION", sql.VarChar, DESCRIPTION);
-        request.input( "AUTHOR_ID", sql.Int, AUTHOR_ID);
-        return request.query( sqlQueries.updateAuthor );
-    };
+   async function updateAuthor( FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION, AUTHOR_ID ) {
+          const pool = await getConnection();
+          const request = await pool.request();
+          request.input("FIRST_NAME", sql.VarChar(250), FIRST_NAME);
+          request.input("LAST_NAME", sql.VarChar(250), LAST_NAME);
+          request.input("NATIONALITY_SHORT", sql.VarChar(5), NATIONALITY_SHORT);
+          request.input("DESCRIPTION", sql.VarChar, DESCRIPTION);
+          request.input("AUTHOR_ID", sql.Int, AUTHOR_ID);
+          return request.query(sqlQueries.updateAuthor);
+     }
 
-    const deleteAuthor = async ( { AUTHOR_ID } ) => {
-        const pool = await getConnection();
-        const request = await pool.request();
-        request.input( "AUTHOR_ID", sql.Int, AUTHOR_ID);
-        return request.query( sqlQueries.deleteAuthor );
-    };
+    async function deleteAuthor( AUTHOR_ID ) {
+          const pool = await getConnection();
+          const request = await pool.request();
+          request.input("AUTHOR_ID", sql.Int, AUTHOR_ID);
+          return request.query(sqlQueries.deleteAuthor);
+     }
 
    return {
         addAuthor,
--- src/routes/api/authors.js
+++ src/routes/api/authors.js
@@ -35,7 +35,7 @@ module.exports.register = async server => {
                 try {
                     const db = request.server.plugins.sql.client;
                     const { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION } = request.payload;
-                    const res = await db.authors.addAuthor( { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION } );
+                    const res = await db.authors.addAuthor( FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION );
                     return res.recordset[ 0 ];
                 } catch ( err ) {
                     console.log( err );
@@ -55,7 +55,7 @@ module.exports.register = async server => {
                 try {
                     const AUTHOR_ID = request.params.AUTHOR_ID;
                     const db = request.server.plugins.sql.client;
-                    const res = await db.authors.deleteAuthor( { AUTHOR_ID } );
+                    const res = await db.authors.deleteAuthor( AUTHOR_ID );
                     return res.recordset[0].AUTHOR_DELETED === true ? "" : boom.notFound();
                 } catch ( err ) {
                     console.log( err );
@@ -76,7 +76,7 @@ module.exports.register = async server => {
                     const AUTHOR_ID = request.params.AUTHOR_ID;
                     const db = request.server.plugins.sql.client;
                     const { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION } = request.payload;
-                    const res = await db.authors.updateAuthor( { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION, AUTHOR_ID } );
+                    const res = await db.authors.updateAuthor( FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION, AUTHOR_ID );
                     const response = { data: res.recordset[0], statusCode: 200};
                     return response;
                 } catch ( err ) {
@@ -85,21 +85,4 @@ module.exports.register = async server => {
             }
         }
     } );
-
-//    server.route( {
-//         method: "POST",
-//         path: "/api/authors",
-//         config: {
-//             handler: async request => {
-//                 try {
-//                     const db = request.server.plugins.sql.client;
-//                     const { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION } = request.payload;
-//                     const res = await db.authors.addAuthor( { FIRST_NAME, LAST_NAME, NATIONALITY_SHORT, DESCRIPTION } );
-//                     return res.recordset[ 0 ];
-//                 } catch ( err ) {
-//                     console.log( err );
-//                 }
-//             }
-//         }
-//     } );
 };
